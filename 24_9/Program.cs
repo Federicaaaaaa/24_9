020cfe0034cceacf4a9ba5cb0f9139e3badf6346
Federicaaaaaa <danger2006.bona@gmail.com>
2023-12-28 17:41:46 +0100

    Aggiungere i file di progetto.

---
+++ 24_9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1 = Vector.Parse(2, 2);
+            Vector v2 = Vector.Parse(3, 3);
+            Vector v3 = Vector.Parse(4, 4);
+
+            Vector vS = v1.Somma(v2);
+            Console.WriteLine("{0}; {1}", v1.vx, v1.vy);
+            Console.WriteLine("{0}; {1}", v2.vx, v2.vy);
+            Console.WriteLine("{0}; {1}", vS.vx, vS.vy);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 24_9/Vector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_9
+{
+    internal class Vector
+    {
+        public Vector()
+        {
+            vx = new int();
+            vy = new int();
+        }
+
+        public int vx { get; set; }
+        public int vy { get; set; }
+
+        public Vector Somma(Vector v)
+        {
+            Vector vR = new Vector();
+            vR.vx = vx + v.vx;
+            vR.vy = vy + v.vy;
+            return vR;
+        }
+
+        public static Vector Parse(int vx, int vy)
+        {
+            Vector v = new Vector();
+            v.vx = vx;
+            v.vy = vy;
+            return v;
+        }
+    }
+}
